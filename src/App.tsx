0e1896f8b08332f8532d8143edef52303a40ea79
Benjamin Benoit <>
2020-05-11 14:56:59 +0200

    app.tsx correction with register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,33 @@
 import React from 'react';
-import Home from './src/views/Home.screen';
-import Login from './src/views/Login.screen';
-import Register from './src/views/Register.screen';
+import Home from './views/Home.screen';
+import Login from './views/Login.screen';
+import Register from './views/Register.screen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import rootThemeStyles from './src/styles/rootTheme.styles';
+import rootThemeStyles from './styles/rootTheme.styles';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 export default function App() {
-    return (
-        <PaperProvider theme={rootThemeStyles}>
-            <NavigationContainer>
-                <Stack.Navigator
-                    initialRouteName="Login"
-                >
-                    <Stack.Screen
-                        name="Home"
-                        component={Home}
-                    />
-                    <Stack.Screen
-                        name="Login"
-                        component={Login}
-                        options={{ headerShown: false }}
-                    />
-                    <Stack.Screen
-                        name="Register"
-                        component={Register}
-                        options={{ headerShown: false }}
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </PaperProvider>
-    );
+	return (
+		<PaperProvider theme={rootThemeStyles}>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="Login">
+					<Stack.Screen name="Home" component={Home} />
+					<Stack.Screen
+						name="Login"
+						component={Login}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="Register"
+						component={Register}
+						options={{ headerShown: false }}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</PaperProvider>
+	);
 }
