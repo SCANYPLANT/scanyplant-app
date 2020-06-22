5c892e37b8fbfa54b59f5453955fd5a66a6fa3ef
Benjamin Benoit <>
2020-06-22 14:38:15 +0200

    homescreen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -11,64 +11,49 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-const isSignedIn = false;
-
-function HomeStack() {
-	return (
-	isSignedIn ? (
-		<>
-		<Stack.Navigator screenOptions={{ headerShown: false }}>
-		  <Stack.Screen name="Home" component={HomeScreen} />
-		</Stack.Navigator>
-		</>
-	  ) : (
-		<>
-			<Stack.Navigator screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="Login" component={LoginScreen} />
-				<Stack.Screen name="Register" component={RegisterScreen} />
-			</Stack.Navigator>
-		</>
-	  )
-	)
-	// return (
-	// 	<Stack.Navigator screenOptions={{ headerShown: false }}>
-	// 		<Stack.Screen name="Login" component={LoginScreen} />
-	// 		<Stack.Screen name="Home" component={HomeScreen} />
-	// 		<Stack.Screen name="Register" component={RegisterScreen} />
-	// 	</Stack.Navigator>
-	// );
-}
+const isSignedIn = true;
 
 export default function Navigation() {
 	return (
 		<NavigationContainer>
-			<Tab.Navigator
-				initialRouteName="Login"
-				screenOptions={({ route }) => ({
-					tabBarIcon: ({ focused, color, size }) => {
-						let iconName;
-						if (route.name === 'Home') {
-							iconName = focused ? 'ios-home' : 'ios-home';
-						} else if (route.name === 'Login') {
-							iconName = focused ? 'md-person' : 'md-person';
-						} else if (route.name === 'Register') {
-							iconName = focused ? 'md-person-add' : 'md-person-add';
-						}
-						return <Ionicons name={iconName} size={size} color={color} />;
-					},
-					tabBarVisible: true,
-				})}
-				tabBarOptions={{
-					activeTintColor: '#ffffff',
-					inactiveTintColor: '#ffffff',
-					inactiveBackgroundColor: '#22577A',
-					activeBackgroundColor: '#29668f',
-				}}
-			>
-				<Tab.Screen name="Home" component={HomeStack} />
-				<Tab.Screen name="Login" component={LoginScreen} />
-				<Tab.Screen name="Register" component={RegisterScreen} />
-			</Tab.Navigator>
+				{
+				isSignedIn ? (
+					<>
+						<Tab.Navigator
+							initialRouteName="Home"
+							screenOptions={({ route }) => ({
+								tabBarIcon: ({ focused, color, size }) => {
+									let iconName;
+									if (route.name === 'Home') {
+										iconName = focused ? 'ios-home' : 'ios-home';
+									} else if (route.name === 'Login') {
+										iconName = focused ? 'md-person' : 'md-person';
+									} else if (route.name === 'Register') {
+										iconName = focused ? 'md-person-add' : 'md-person-add';
+									}
+									return <Ionicons name={iconName} size={size} color={color} />;
+								},
+								tabBarVisible: true,
+							})}
+							tabBarOptions={{
+								activeTintColor: '#ffffff',
+								inactiveTintColor: '#ffffff',
+								inactiveBackgroundColor: '#22577A',
+								activeBackgroundColor: '#29668f',
+							}}
+						>
+							<Tab.Screen name="Home" component={HomeScreen} />
+						</Tab.Navigator>
+					</>
+				) : (
+					<>
+						<Stack.Navigator screenOptions={{ headerShown: false }}>
+							<Stack.Screen name="Login" component={LoginScreen} />
+							<Stack.Screen name="Register" component={RegisterScreen} />
+						</Stack.Navigator>
+					</>
+				)
+				}
 		</NavigationContainer>
 	);
 }
