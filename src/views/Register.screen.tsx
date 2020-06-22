78edea074897065eb901fac1b6783dc7c4f0645c
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-06-22 16:39:24 +0200

    fix error

--- src/views/Register.screen.tsx
+++ src/views/Register.screen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, Card, TextInput } from 'react-native-paper';
+import { Button, Card, TextInput, Text } from 'react-native-paper';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import { useFormik } from 'formik';
+import { Formik, useFormik } from 'formik';
 import * as Yup from 'yup';
 import CardContent from 'react-native-paper/lib/typescript/src/components/Card/CardContent';
 
@@ -17,76 +17,84 @@ const styles = StyleSheet.create({
 
 export default function RegisterScreen({navigation}) {
 
-    const userForm = useFormik({
-        initialValues: {
-            nom: "",
-            prenom: "",
-            email: "",
-            password: "",
-            confirmPassword: "",
-        },
-        validationSchema: Yup.object().shape({
-            nom: Yup.string().required("This field is required"),
-            prenom: Yup.string().required("This field is required"),
-            email: Yup.string().email().required("This field is required"),
-            password: Yup.string().required("This field is required"),
-            confirmPassword: Yup.string().when("password", {
-                is: val => (val && val.length > 0 ? true : false),
-                then: Yup.string().oneOf(
-                    [Yup.ref("password")],
-                    "Both password need to be the same"
-                )
-            })
-        }),
-        onSubmit: values => {
-           console.log(values)
-        }
-    })
-
     return (
         <SafeAreaView>
-                <CardContent>
+            <Formik
+                initialValues={{
+                    nom: "",
+                    prenom: "",
+                    email: "",
+                    password: "",
+                    confirmPassword: "",
+                }}
+                validationSchema={Yup.object().shape({
+                    nom: Yup.string().required("This field is required"),
+                    prenom: Yup.string().required("This field is required"),
+                    email: Yup.string().email().required("This field is required"),
+                    password: Yup.string().required("This field is required"),
+                    confirmPassword: Yup.string().when("password", {
+                        is: val => (val && val.length > 0 ? true : false),
+                        then: Yup.string().oneOf(
+                            [Yup.ref("password")],
+                            "Both password need to be the same"
+                        )
+                    })
+                })}
+                onSubmit={values => console.log(values)}
+            >
+                {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+                <Card.Content>
                     <TextInput
                         label='Nom'
-                        value={ userForm.values.nom}
-                        onBlur={userForm.handleBlur}
-                        onChangeText={userForm.handleChange}
+                        value={ values.nom}
+                        onBlur={handleBlur}
+                        onChangeText={handleChange('nom')}
                     />
-                    {/*<span className="error" style={{color: "red"}}>{userForm.errors.nom}</span>*/}
+                    {errors.nom &&
+                    <Text style={{ fontSize: 10, color: 'red' }}>{errors.nom}</Text>
+                    }
                     <TextInput
                         label='Prenom'
-                        value={ userForm.values.prenom}
-                        onBlur={userForm.handleBlur}
-                        onChangeText={userForm.handleChange}
+                        value={ values.prenom}
+                        onBlur={handleBlur}
+                        onChangeText={handleChange('prenom')}
                     />
-                    {/*<span className="error" style={{color: "red"}}>{userForm.errors.prenom}</span>*/}
+                    {errors.prenom &&
+                    <Text style={{ fontSize: 10, color: 'red' }}>{errors.prenom}</Text>
+                    }
                     <TextInput
                         label='Email'
-                        value={ userForm.values.email}
-                        onBlur={userForm.handleBlur}
-                        onChangeText={userForm.handleChange}
+                        value={ values.email}
+                        onBlur={handleBlur}
+                        onChangeText={handleChange('email')}
                     />
-                    {/*<span className="error" style={{color: "red"}}>{userForm.errors.email}</span>*/}
+                    {errors.email &&
+                    <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
+                    }
                     <TextInput
                         label='Password'
-                        value={ userForm.values.password}
-                        onBlur={userForm.handleBlur}
-                        onChangeText={userForm.handleChange}
+                        value={ values.password}
+                        onBlur={handleBlur}
+                        onChangeText={handleChange('password')}
                     />
-                    {/*<span className="error" style={{color: "red"}}>{userForm.errors.password}</span>*/}
+                    {errors.password &&
+                    <Text style={{ fontSize: 10, color: 'red' }}>{errors.password}</Text>
+                    }
                     <TextInput
                         label='confirm Password'
-                        value={ userForm.values.confirmPassword}
-                        onBlur={userForm.handleBlur}
-                        onChangeText={userForm.handleChange}
+                        value={ values.confirmPassword}
+                        onBlur={handleBlur}
+                        onChangeText={handleChange('confirmPassword')}
                     />
-                    {/*<span className="error" style={{color: "red"}}>{userForm.errors.confirmPassword}</span>*/}
-                    <Button  disabled={!userForm.isValid}>
-                        Register
-                    </Button>
-                </CardContent>
-                <Button style={styles.button} mode="contained" >Cancel</Button>
-                <Button style={styles.button} mode="contained" onPress={navigation.navigate('Login')}  >Login</Button>
+                    {errors.confirmPassword &&
+                    <Text style={{ fontSize: 10, color: 'red' }}>{errors.confirmPassword}</Text>
+                    }
+                    <Button disabled={!isValid}>Register</Button>
+                </Card.Content>
+                )}
+            </Formik>
+                {/*<Button style={styles.button} mode="contained" >Cancel</Button>*/}
+                {/*<Button style={styles.button} mode="contained" onPress={navigation.navigate('Login')}  >Login</Button>*/}
         </SafeAreaView>
     );
 }
