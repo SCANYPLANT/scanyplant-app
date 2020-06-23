980a00d0ad33817f6236602c016b8f62f87615d5
Benjamin Benoit <>
2020-06-23 16:10:51 +0200

    profil

---
+++ src/views/Profil.screen.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Button, Card, TextInput, Text } from 'react-native-paper';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { Formik, useFormik } from 'formik';
+import * as Yup from 'yup';
+import { AppBar } from '../components';
+
+const styles = StyleSheet.create({
+    button: {
+        textDecorationColor: "#ffffff",
+        marginLeft: 20,
+        marginRight: 20,
+        marginVertical: 10
+    },
+});
+
+export default function ProfilScreen({ navigation }) {
+
+    return (
+        <SafeAreaView>
+            <AppBar title ='MON COMPTE'/>
+            <>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string().email().required("This field is required"),
+                    password: Yup.string().required("This field is required"),
+                })}
+                onSubmit={values => console.log(values)}
+            >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+                    <>
+                        <Text>Bienvenue xxx</Text>
+                        <TextInput
+                            label='Nom'
+                            value={values.lastName}
+                            onBlur={handleBlur('lastName')}
+                            onChangeText={e => handleChange('lastName')}
+                        />
+                        {errors.lastName &&
+                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.lastName}</Text>
+                        }
+                        <TextInput
+                            label='Prenom'
+                            value={values.firstName}
+                            onBlur={handleBlur('firstName')}
+                            onChangeText={e =>handleChange('firstName')}
+                        />
+                        {errors.firstName &&
+                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.firstName}</Text>
+                        }
+                        <Button onPress={() => navigation.navigate('ChangePassword')} >Modifier mon mot de passe</Button>
+                    </>
+                    )}
+                </Formik>
+                <Button  onPress={() => navigation.navigate('changePassword')} mode="contained" ><Text>Enregistrer</Text></Button>
+                <Button style={styles.button} onPress={() => navigation.navigate('Register')}>Me deconnecter</Button>
+                <Button style={styles.button} onPress={() => navigation.navigate('Register')}>Me désinscrire</Button>
+            </>
+        </SafeAreaView>
+    );
+}
