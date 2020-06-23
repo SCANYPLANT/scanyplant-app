eeb31cdb61045b9f94bb1386e38f7aac46f7a8f8
Benjamin Benoit <>
2020-06-23 10:39:06 +0200

    login

--- src/views/Login.screen.tsx
+++ src/views/Login.screen.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Button, Card, TextInput, Text } from 'react-native-paper';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { Formik, useFormik } from 'formik';
+import * as Yup from 'yup';
+import { AppBar } from '../components';
 
 const styles = StyleSheet.create({
     button: {
@@ -15,18 +18,46 @@ export default function LoginScreen({ navigation }) {
 
     return (
         <SafeAreaView>
-                <Card.Content>
-                    <TextInput
-                        label='Email'
-                    />
-                    <TextInput
-                        label='Password'
-                    />
-                </Card.Content>
-
-                <Button style={styles.button} mode="contained" ><Text>Login</Text></Button>
+            <AppBar title ='LOGIN'/>
+            <>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string().email().required("This field is required"),
+                    password: Yup.string().required("This field is required"),
+                })}
+                onSubmit={values => console.log(values)}
+            >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+                    <>
+                        <TextInput
+                            label='Email'
+                            value={ values.email}
+                            onBlur={handleBlur}
+                            onChangeText={handleChange('email')}
+                        />
+                        {errors.email &&
+                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
+                        }
+                        <TextInput
+                            label='Password'
+                            value={ values.password}
+                            onBlur={handleBlur}
+                            onChangeText={handleChange('password')}
+                        />
+                        {errors.password &&
+                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.password}</Text>
+                        }
+                        <Button disabled={!isValid} style={styles.button} mode="contained" ><Text>Login</Text></Button>
+                    </>
+                    )}
+                </Formik>
                 <Button  onPress={() => navigation.navigate('changePassword')} >Password forgotten</Button>
-                <Button style={styles.button} mode="contained" onPress={() => navigation.navigate('Register')}><Text>Register</Text></Button>
-            </SafeAreaView>
+                <Button style={styles.button} onPress={() => navigation.navigate('Register')}>Register</Button>
+            </>
+        </SafeAreaView>
     );
 }
