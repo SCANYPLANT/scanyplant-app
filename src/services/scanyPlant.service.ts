121c833a1766322f56708bbb7ac837bef506dc97
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-06-29 13:32:14 +0200

    add react naitve dotenv

--- src/services/scanyPlant.service.ts
+++ src/services/scanyPlant.service.ts
@@ -1,40 +1,46 @@
 import { AsyncStorage } from 'react-native';
+import { REACT_APP_API_URL } from 'react-native-dotenv';
 
 export const scanyPlantService = {
-    getAll
+	getAll,
 };
 
 function getAll() {
-    const requestOptions = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
-    };
+	const requestOptions = {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+		},
+	};
 
-    return fetch(`https://urgence-api.herokuapp.com/api/hospital`, requestOptions)
-        .then(handleResponse)
-        .then(async result => result.records);
+	return fetch(REACT_APP_API_URL as string, requestOptions)
+		.then(handleResponse)
+		.then(async result => result.records);
 }
 
 async function logout() {
-    await AsyncStorage.removeItem('user');
+	await AsyncStorage.removeItem('user');
 }
 
-function handleResponse(response: { text: () => Promise<any>; ok: any; status: number; statusText: any; }) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if (response.status === 401) {
-                // auto logout if 401 response returned from api
-                logout().then(r => r);
-                window.location.reload();
-            }
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
+function handleResponse(response: {
+	text: () => Promise<any>;
+	ok: any;
+	status: number;
+	statusText: any;
+}) {
+	return response.text().then(text => {
+		const data = text && JSON.parse(text);
+		if (!response.ok) {
+			if (response.status === 401) {
+				// auto logout if 401 response returned from api
+				logout().then(r => r);
+				window.location.reload();
+			}
+			const error = (data && data.message) || response.statusText;
+			return Promise.reject(error);
+		}
 
-        return data;
-    });
+		return data;
+	});
 }
--- src/views/Register.screen.tsx
+++ src/views/Register.screen.tsx
@@ -8,131 +8,153 @@ import { AppBar } from '../components';
 import { upperCase } from 'lodash';
 
 const styles = StyleSheet.create({
-    button: {
-        textDecorationColor: '#ffffff',
-        marginLeft: 20,
-        marginRight: 20,
-        marginVertical: 10
-    },
-    textSpan: {
-        fontSize: 20,
-        color: '#c7f9cc',
-        flex: 1,
-        alignItems: 'center',
-        textAlign: 'center'
-    },
+	button: {
+		textDecorationColor: '#ffffff',
+		marginLeft: 20,
+		marginRight: 20,
+		marginVertical: 10,
+	},
+	textSpan: {
+		fontSize: 20,
+		color: '#c7f9cc',
+		flex: 1,
+		alignItems: 'center',
+		textAlign: 'center',
+	},
 });
 
 export default function RegisterScreen({ navigation }) {
-    const registerUser = (body) => {
-        // console.log('fetch');
-        console.log('body ====>', process.env.REACT_APP_API_URL);
-        fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body),
-        })
-            .then(response => response.json())
-            .then(result => {
-                console.log('result', result);
-                navigation.navigate('Login');
-            })
-            .catch(err => console.log(err));
-    };
-    return (
-        <>
-            <AppBar title='REGISTER'/>
-            <>
-                <Formik
-                    initialValues={{
-                        firstName: '',
-                        lastName: '',
-                        email: '',
-                        password: '',
-                        confirmPassword: '',
-                    }}
-                    validationSchema={Yup.object().shape({
-                        lastName: Yup.string().required('This field is required'),
-                        firstName: Yup.string().required('This field is required'),
-                        email: Yup.string().email().required('This field is required'),
-                        password: Yup.string().required('This field is required'),
-                        confirmPassword: Yup.string().when('password', {
-                            is: val => (val && val.length > 0 ? true : false),
-                            then: Yup.string().oneOf(
-                                [Yup.ref('password')],
-                                'Both password need to be the same'
-                            )
-                        })
-                    })}
-                    onSubmit={values => {
-                        return registerUser({
-                            email: upperCase(values.email),
-                            firstName: values.firstName,
-                            lastName: values.lastName,
-                            password: values.password,
-                        });
-                    }}
-                >
-                    {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
-                        <>
-                            <TextInput
-                                label='Nom'
-                                value={values.lastName}
-                                onBlur={handleBlur('lastName')}
-                                onChangeText={handleChange('lastName')}
-                            />
-                            {errors.lastName &&
-                            <Text style={{ fontSize: 10, color: 'red' }}>{errors.lastName}</Text>
-                            }
-                            <TextInput
-                                label='Prenom'
-                                value={values.firstName}
-                                onBlur={handleBlur('firstName')}
-                                onChangeText={handleChange('firstName')}
-                            />
-                            {errors.firstName &&
-                            <Text style={{ fontSize: 10, color: 'red' }}>{errors.firstName}</Text>
-                            }
-                            <TextInput
-                                label='Email'
-                                value={values.email}
-                                onBlur={handleBlur('email')}
-                                onChangeText={handleChange('email')}
-                            />
-                            {errors.email &&
-                            <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
-                            }
-                            <TextInput
-                                label='Password'
-                                value={values.password}
-                                onBlur={handleBlur('password')}
-                                secureTextEntry={true}
-                                onChangeText={handleChange('password')}
-                            />
-                            {errors.password &&
-                            <Text style={{ fontSize: 10, color: 'red' }}>{errors.password}</Text>
-                            }
-                            <TextInput
-                                label='confirm Password'
-                                value={values.confirmPassword}
-                                onBlur={handleBlur('confirmPassword')}
-                                secureTextEntry={true}
-                                onChangeText={handleChange('confirmPassword')}
-                            />
-                            {errors.confirmPassword &&
-                            <Text style={{ fontSize: 10, color: 'red' }}>{errors.confirmPassword}</Text>
-                            }
-                            <Button disabled={!isValid} style={styles.button} mode="contained"
-                                    onPress={handleSubmit}>Register</Button>
-                        </>
-                    )}
-                </Formik>
-                <Button onPress={() => navigation.navigate('Login')}>Login</Button>
-            </>
-        </>
-    );
+	const registerUser = body => {
+		// console.log('fetch');
+		console.log('body ====>', process.env);
+		fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(body),
+		})
+			.then(response => response.json())
+			.then(result => {
+				console.log('result', result);
+				navigation.navigate('Login');
+			})
+			.catch(err => console.log(err));
+	};
+	return (
+		<>
+			<AppBar title="REGISTER" />
+			<>
+				<Formik
+					initialValues={{
+						firstName: '',
+						lastName: '',
+						email: '',
+						password: '',
+						confirmPassword: '',
+					}}
+					validationSchema={Yup.object().shape({
+						lastName: Yup.string().required('This field is required'),
+						firstName: Yup.string().required('This field is required'),
+						email: Yup.string().email().required('This field is required'),
+						password: Yup.string().required('This field is required'),
+						confirmPassword: Yup.string().when('password', {
+							is: val => (val && val.length > 0 ? true : false),
+							then: Yup.string().oneOf(
+								[Yup.ref('password')],
+								'Both password need to be the same',
+							),
+						}),
+					})}
+					onSubmit={values => {
+						return registerUser({
+							email: upperCase(values.email),
+							firstName: values.firstName,
+							lastName: values.lastName,
+							password: values.password,
+						});
+					}}
+				>
+					{({
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						values,
+						errors,
+						isValid,
+					}) => (
+						<>
+							<TextInput
+								label="Nom"
+								value={values.lastName}
+								onBlur={handleBlur('lastName')}
+								onChangeText={handleChange('lastName')}
+							/>
+							{errors.lastName && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.lastName}
+								</Text>
+							)}
+							<TextInput
+								label="Prenom"
+								value={values.firstName}
+								onBlur={handleBlur('firstName')}
+								onChangeText={handleChange('firstName')}
+							/>
+							{errors.firstName && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.firstName}
+								</Text>
+							)}
+							<TextInput
+								label="Email"
+								value={values.email}
+								onBlur={handleBlur('email')}
+								onChangeText={handleChange('email')}
+							/>
+							{errors.email && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.email}
+								</Text>
+							)}
+							<TextInput
+								label="Password"
+								value={values.password}
+								onBlur={handleBlur('password')}
+								secureTextEntry={true}
+								onChangeText={handleChange('password')}
+							/>
+							{errors.password && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.password}
+								</Text>
+							)}
+							<TextInput
+								label="confirm Password"
+								value={values.confirmPassword}
+								onBlur={handleBlur('confirmPassword')}
+								secureTextEntry={true}
+								onChangeText={handleChange('confirmPassword')}
+							/>
+							{errors.confirmPassword && (
+								<Text style={{ fontSize: 10, color: 'red' }}>
+									{errors.confirmPassword}
+								</Text>
+							)}
+							<Button
+								disabled={!isValid}
+								style={styles.button}
+								mode="contained"
+								onPress={handleSubmit}
+							>
+								Register
+							</Button>
+						</>
+					)}
+				</Formik>
+				<Button onPress={() => navigation.navigate('Login')}>Login</Button>
+			</>
+		</>
+	);
 }
-
