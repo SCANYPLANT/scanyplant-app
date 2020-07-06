a3d6fde6d25e4c8bcc34f85f4cc775c02f560a7d
Benjamin Benoit <>
2020-07-06 17:26:53 +0200

    plant list

--- src/views/PlantsList.screen.tsx
+++ src/views/PlantsList.screen.tsx
@@ -1,30 +1,32 @@
 import React, { useEffect } from 'react';
-import { Image, ScrollView, StyleSheet, View } from 'react-native';
-import { Card, Text } from 'react-native-paper';
+import { ScrollView, StyleSheet, View, Dimensions, Image } from 'react-native';
+import { Card, Text, Avatar, Title, Chip } from 'react-native-paper';
 import { AppBar } from '../components';
 import { useDispatch, useSelector } from 'react-redux';
 import { plantActions } from '../actions';
+import Carousel from 'react-native-snap-carousel';
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        marginTop: 20,
-    },
-    header: {
-        flex: 1,
-        justifyContent: 'center',
-        paddingHorizontal: 50,
-    },
-    body: {
-        flex: 3,
+        marginTop: 20
     },
     item: {
-        flex: 1,
         marginHorizontal: 20,
         marginVertical: 10,
-        paddingVertical: 10,
-        paddingHorizontal: 10,
         justifyContent: 'center',
+    },
+    detailLabel: {
+        height: 30,
+        width: '100%',
+        backgroundColor: '#57CC99',
+        color: '#ffffff',
+        paddingLeft: 5,
+        fontSize: 17
+    },
+    viewColumn: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'stretch'
     },
 });
 
@@ -38,6 +40,19 @@ export default function PlantsListScreen({ navigation }) {
         // console.log('plant click:', plant.fields.raison_sociale);
         navigation.navigate('Discover', { query: plant });
     };
+
+    const SLIDER_WIDTH = Dimensions.get('window').width;
+    const SLIDER_HEIGHT = Dimensions.get('window').height;
+    const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
+    const ITEM_HEIGHT = Math.round(SLIDER_HEIGHT * 0.2);
+    const emptyInfos = 'Aucune donnée';
+    const renderItem = ({ item, index }) =>
+        <Image source={{ uri: item?.url }} style={{
+            width: ITEM_WIDTH,
+            height: ITEM_HEIGHT,
+            marginBottom: 20
+        }}/>
+    ;
 
     return (
         <>
@@ -53,14 +68,48 @@ export default function PlantsListScreen({ navigation }) {
                                         accessibilityStates
                                         style={styles.item}
                                         key={plant.recordid}
-                                        onPress={() => plantClick(plant)}
                                     >
-                                        <Card.Content>
-                                            <Image
-                                                source={plant.images}
-                                                style={{ width: 16, height: 16, borderRadius: 10 }}
-                                            />
-                                            <Text accessibilityStates>{plant.name}</Text>
+                                        <Card.Content
+                                            style={{
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                                width: '100%',
+                                                padding: 0,
+                                                margin: 0
+                                            }}>
+                                            <View style={{marginBottom: 20}}>
+                                                {plant && plant.images == null &&  <Avatar.Icon
+                                                    accessibilityStates
+                                                    icon={'flower'}
+                                                    size={100}
+                                                    style={{
+                                                        width: 100,
+                                                        height: 100,
+                                                        borderRadius: 10,
+                                                        display: 'flex',
+                                                        alignSelf: 'center',
+                                                    }}
+                                                /> }
+                                                {plant && plant.images && <Carousel
+                                                    data={plant.images}
+                                                    layout={'stack'}
+                                                    renderItem={renderItem}
+                                                    sliderWidth={SLIDER_WIDTH}
+                                                    sliderHeight={ITEM_HEIGHT}
+                                                    itemWidth={ITEM_WIDTH}
+                                                />}
+                                            </View>
+                                            {/* <Avatar.Icon
+                                                accessibilityStates
+                                                icon={'flower'}
+                                                style={{ width: '50%', borderRadius: 0, left: '25%', marginBottom: 10 }}
+                                            /> */}
+                                            <Chip accessibilityStates icon="flower" style={{backgroundColor:'#57CC99', margin: 5}}>{ plant.name }</Chip>
+                                            <Chip accessibilityStates icon="calendar-range" style={{margin: 5}}>Ajouté le: {plant.createdAt}</Chip>
+                                            <Chip accessibilityStates icon="white-balance-sunny" style={{backgroundColor:'#f5da6e', margin: 5}}>Luminosité: {plant.brightness}</Chip>
+                                            <Chip accessibilityStates icon="water-outline" style={{backgroundColor:'#6ec3f5', margin: 5}}>Prochain arrosage dans {plant.shift} jour(s)</Chip>
+                                            <Chip accessibilityStates icon="repeat" style={{margin: 5}}>Repeter tous les {plant.repetition} jours</Chip>
+                                            <Text accessibilityStates></Text>
                                         </Card.Content>
                                     </Card>
                                 );
